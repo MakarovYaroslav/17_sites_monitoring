a4dbd884c23b12b14632cdbb39e1804c317df247
MakarovYaroslav <yaroslav.makarov.97@mail.ru>
2016-11-14 21:46:21 +0400

    файл для проверки в аргумент, вывод результатов
    путь к файлу с адресами для проверки - параметр скрипта, улучшен пользовательский интерфейс

--- check_sites_health.py
+++ check_sites_health.py
@@ -2,6 +2,7 @@ import pythonwhois
 import requests
 from tld import get_tld
 from datetime import datetime
+import argparse
 
 
 def load_urls4check(path):
@@ -10,11 +11,8 @@ def load_urls4check(path):
 
 
 def is_server_respond_with_200(url):
-    response = requests.get(url)
-    if response.status_code == 200:
-        return True
-    else:
-        return False
+    response = requests.get(url.rstrip('\n'))
+    return 0 if response.status_code == 200 else response.status_code
 
 
 def get_domain_expiration_date(domain_name):
@@ -24,20 +22,22 @@ def get_domain_expiration_date(domain_name):
 
 if __name__ == '__main__':
     days_amount_4check = 31
-    urls = load_urls4check('urls.txt')
+    parser = argparse.ArgumentParser()
+    parser.add_argument("filepath",
+                        help="путь к файлу с URL адресами для проверки")
+    args = parser.parse_args()
+    urls = load_urls4check(args.filepath)
     for url in urls:
-        url = url.rstrip('\n')
-        print('\nИнформация по %s:' % url)
+        is_OK = True
         if is_server_respond_with_200(url):
-            print('Cервер отвечает на запрос статусом HTTP 200')
-        else:
-            print('Cервер НЕ отвечает на запрос статусом HTTP 200')
+            is_OK = False
+            print(url + 'FAIL!\nCервер НЕ отвечает на запрос статусом 200')
         days_between_dates = (
-            get_domain_expiration_date(get_tld(url))-datetime.today()
+            get_domain_expiration_date(get_tld(url)) - datetime.today()
         ).days
-        if days_between_dates > days_amount_4check:
-            print('Доменное имя сайта проплачено минимум на %s день вперед'
+        if days_between_dates < days_amount_4check:
+            is_OK = False
+            print(url + 'FAIL!\nДоменное имя сайта НЕ проплачено на %s день'
                   % days_amount_4check)
-        else:
-            print('Доменное имя сайта НЕ проплачено минимум на %s день вперед'
-                  % days_amount_4check)
+        if is_OK:
+            print("OK!")
