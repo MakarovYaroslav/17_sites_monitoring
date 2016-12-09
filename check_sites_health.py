5bc3de1e081112997abf110bb12e2753b09391a6
MakarovYaroslav <yaroslav.makarov.97@mail.ru>
2016-12-09 21:44:57 +0400

    Вывод результатов в отдельной функции

--- check_sites_health.py
+++ check_sites_health.py
@@ -12,12 +12,33 @@ def load_urls4check(path):
 
 def is_server_respond_with_200(url):
     response = requests.get(url.rstrip('\n'))
-    return 0 if response.status_code == 200 else response.status_code
+    return True if response.status_code == 200 else False
 
 
 def get_domain_expiration_date(domain_name):
     details = pythonwhois.get_whois(domain_name)
-    return details['expiration_date'][0]
+    expiration_date = details['expiration_date']
+    if type(expiration_date) is list:
+        return expiration_date[0]
+    else:
+        return expiration_date
+
+
+def print_result(urls):
+    for url in urls:
+        is_ok = True
+        if not is_server_respond_with_200(url):
+            is_ok = False
+            print(url + 'FAIL!\nCервер НЕ отвечает на запрос статусом 200\n')
+        days_between_dates = (
+            get_domain_expiration_date(get_tld(url)) - datetime.today()
+        ).days
+        if days_between_dates < days_amount_4check:
+            is_ok = False
+            print(url + 'FAIL!\nДоменное имя сайта НЕ проплачено на %s день\n'
+                  % days_amount_4check)
+        if is_ok:
+            print("OK!\n")
 
 
 if __name__ == '__main__':
@@ -26,18 +47,5 @@ if __name__ == '__main__':
     parser.add_argument("filepath",
                         help="путь к файлу с URL адресами для проверки")
     args = parser.parse_args()
-    urls = load_urls4check(args.filepath)
-    for url in urls:
-        is_ok = True
-        if is_server_respond_with_200(url):
-            is_ok = False
-            print(url + 'FAIL!\nCервер НЕ отвечает на запрос статусом 200')
-        days_between_dates = (
-            get_domain_expiration_date(get_tld(url)) - datetime.today()
-        ).days
-        if days_between_dates < days_amount_4check:
-            is_ok = False
-            print(url + 'FAIL!\nДоменное имя сайта НЕ проплачено на %s день'
-                  % days_amount_4check)
-        if is_ok:
-            print("OK!")
+    site_urls = load_urls4check(args.filepath)
+    print_result(site_urls)
